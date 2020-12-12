b57bda48e5f23faec6f85de6518041790b1ef203
sidrai97 <siddhantraisgr@gmail.com>
2020-12-12 20:05:42 +0530

    rest api exercise code

---
+++ course-02/exercises/udacity-c2-restapi/prod.profile
@@ -0,0 +1,8 @@
+export POSTGRES_HOST=udagramraidev.cdniksvigdna.us-east-2.rds.amazonaws.com
+export POSTGRES_DATABASE=udagramdev
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=udagramraidev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export AWS_MEDIA_BUCKET=udagramraidev
+export JWT_SECRET=sidrai97
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,46 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const items = await FeedItem.findByPk(id);
+    if(items === null){
+        return res.status(404).send({message: 'Not found'})
+    }
+    else{
+        return res.status(200).send(items);
+    }
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const { id } = req.params;
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+    
+        // check Caption is valid
+        if (!caption) {
+            return res.status(400).send({ message: 'Caption is required or malformed' });
+        }
+    
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({ message: 'File url is required' });
+        }
+
+        let item = await FeedItem.findByPk(id);
+        if(item === null){
+            return res.status(404).send({message: 'Resource not found'});
+        }
+        item.caption = caption;
+        item.url = fileName;
+
+        const saved_item = await item.save();
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        return res.status(200).send(saved_item);
 });
 
 
